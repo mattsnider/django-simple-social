eb5c5a354c3588f061a61989320a417378911fe4
matt snider <msnider@votizen.com>
2012-11-02 12:24:07 -0700

    removing README and modifying the distribution to logic to create it temporarily from README.md

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
-from os import path
+import os, sys
 
-ROOT_DIR = path.abspath(path.dirname(__file__))
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 def get_deps():
-    f = open(path.join(ROOT_DIR, "requirements.pip"), 'r')
+    f = open(os.path.join(ROOT_DIR, "requirements.pip"), 'r')
     return [l[:-1] for l in f.readlines()]
+
+# README is required for distribution, but README.md is required for github,
+#   so create README temporarily
+is_distribution = 'sdist' in sys.argv or 'bdist_wininst' in sys.argv
+
+if is_distribution:
+    os.system('cp %s/README.md %s/README' % (ROOT_DIR, ROOT_DIR))
 
 sdict = dict(
     name = 'django-simple-social',
@@ -13,7 +20,7 @@ sdict = dict(
     version='.'.join(map(str, __import__('django_simple_social').__version__)),
     description = 'A generic system for interacting with remote APIs '
                   'that need to create Django socials.',
-    long_description=open('README').read(),
+    long_description=open('README.md').read(),
     url = 'https://github.com/mattsnider/Django-Simple-Social',
     author = 'Matt Snider',
     author_email = 'admin@mattsnider.com',
@@ -35,3 +42,7 @@ sdict = dict(
 
 from distutils.core import setup
 setup(**sdict)
+
+# cleanup README
+if is_distribution:
+    os.remove('%s/README' % ROOT_DIR)
